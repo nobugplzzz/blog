20dbf32214144cbba91ccb8ca43fa8e334c9babe
luqiyu <316882012@qq.com>
2021-06-09 20:46:02 +0800

    日志管理，切面类打印操作日志实现打印部分字段

--- qiyu-blog-springboot/src/main/java/com/luqiyu/qiyublogspringboot/handler/OptLogAspect.java
+++ qiyu-blog-springboot/src/main/java/com/luqiyu/qiyublogspringboot/handler/OptLogAspect.java
@@ -1,94 +1,95 @@
 package com.luqiyu.qiyublogspringboot.handler;
-//
+
 //import com.alibaba.fastjson.JSON;
-//import com.luqiyu.qiyublogspringboot.annotation.OptLog;
-//import com.luqiyu.qiyublogspringboot.entity.OperationLog;
-//import com.luqiyu.qiyublogspringboot.mapper.OperationLogMapper;
+import com.luqiyu.qiyublogspringboot.annotation.OptLog;
+import com.luqiyu.qiyublogspringboot.entity.OperationLog;
+import com.luqiyu.qiyublogspringboot.mapper.OperationLogMapper;
 //import com.luqiyu.qiyublogspringboot.util.IpUtil;
 //import com.luqiyu.qiyublogspringboot.util.UserUtil;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import org.aspectj.lang.JoinPoint;
-//import org.aspectj.lang.annotation.AfterReturning;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.aspectj.lang.reflect.MethodSignature;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.scheduling.annotation.Async;
-//import org.springframework.stereotype.Component;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.web.context.request.RequestAttributes;
-//import org.springframework.web.context.request.RequestContextHolder;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import java.lang.reflect.Method;
-//import java.util.Date;
-//import java.util.Objects;
-//
-///**
-// * 操作日志切面
-// *
-// * @author: 启誉
-// * @create: 2021-06-08
-// **/
-//@Aspect // 创建一个AOP切面类
-//@Component
-//public class OptLogAspect {
-//
-//    @Autowired
-//    OperationLogMapper operationLogMapper;
-//
-//    /**
-//     * 设置操作日志切入点 记录操作日志 在注解的位置切入代码
-//     */
-//    @Pointcut("@annotation(com.luqiyu.qiyublogspringboot.annotation.OptLog)")
-//    public void optLogPointCut() {
-//    }
-//
-//    /**
-//     * 正常返回通知，拦截用户操作日志，连接点正常执行完成后执行， 如果连接点抛出异常，则不会执行
-//     *
-//     * @param joinPoint 切入点
-//     * @param keys      返回结果
-//     */
-//    @Async
-//    @Transactional(rollbackFor = Exception.class)
-//    @AfterReturning(value = "optLogPointCut()", returning = "keys")
-//    public void saveOptLog(JoinPoint joinPoint, Object keys) {
-//        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
-//        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
-//        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
-//        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
-//        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
-//        // 获取RequestAttributes
-//        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-//        // 从获取RequestAttributes中获取HttpServletRequest的信息
-//        HttpServletRequest request = (HttpServletRequest) Objects.requireNonNull(requestAttributes).resolveReference(RequestAttributes.REFERENCE_REQUEST);
-//        OperationLog operationLog = new OperationLog();
-//        // 从切面织入点处通过反射机制获取织入点处的方法
-//        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-//        // 获取切入点所在的方法
-//        Method method = signature.getMethod();
-//        // 获取操作
-//        Api api = (Api) signature.getDeclaringType().getAnnotation(Api.class);
-//        ApiOperation apiOperation = method.getAnnotation(ApiOperation.class);
-//        OptLog optLog = method.getAnnotation(OptLog.class);
-//        // 操作模块
-//        operationLog.setOptModule(api.tags()[0]);
-//        // 操作类型
-//        operationLog.setOptType(optLog.optType());
-//        // 操作描述
-//        operationLog.setOptDesc(apiOperation.value());
-//        // 获取请求的类名
-//        String className = joinPoint.getTarget().getClass().getName();
-//        // 获取请求的方法名
-//        String methodName = method.getName();
-//        methodName = className + "." + methodName;
-//        // 请求方式
-//        operationLog.setRequestMethod(Objects.requireNonNull(request).getMethod());
-//        // 请求方法
-//        operationLog.setOptMethod(methodName);
-//        // 请求参数
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * 操作日志切面类
+ *
+ * @author: 启誉
+ * @create: 2021-06-08
+ **/
+@Aspect // 创建一个AOP切面类
+@Component
+public class OptLogAspect {
+
+    @Autowired
+    OperationLogMapper operationLogMapper;
+
+    /**
+     * 设置操作日志切入点 记录操作日志 在注解的位置切入代码
+     */
+    @Pointcut("@annotation(com.luqiyu.qiyublogspringboot.annotation.OptLog)")
+    public void optLogPointCut() {
+    }
+
+    /**
+     * 正常返回通知，拦截用户操作日志，连接点正常执行完成后执行， 如果连接点抛出异常，则不会执行
+     *
+     * @param joinPoint 切入点
+     * @param keys      返回结果
+     */
+    @Async
+    @Transactional(rollbackFor = Exception.class)
+    @AfterReturning(value = "optLogPointCut()", returning = "keys")
+    public void saveOptLog(JoinPoint joinPoint, Object keys) {
+        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
+        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
+        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
+        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
+        System.out.println("+++++++++++++++++++++++++++\n\n\n\n\n\n");
+        // 获取RequestAttributes
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        // 从获取RequestAttributes中获取HttpServletRequest的信息
+        HttpServletRequest request = (HttpServletRequest) Objects.requireNonNull(requestAttributes).resolveReference(RequestAttributes.REFERENCE_REQUEST);
+        OperationLog operationLog = new OperationLog();
+        // 从切面织入点处通过反射机制获取织入点处的方法
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        // 获取切入点所在的方法
+        Method method = signature.getMethod();
+        // 获取操作
+        Api api = (Api) signature.getDeclaringType().getAnnotation(Api.class);
+        ApiOperation apiOperation = method.getAnnotation(ApiOperation.class);
+        OptLog optLog = method.getAnnotation(OptLog.class);
+        // 操作模块
+        operationLog.setOptModule(api.tags()[0]);
+        // 操作类型
+        operationLog.setOptType(optLog.optType());
+        // 操作描述
+        operationLog.setOptDesc(apiOperation.value());
+        // 获取请求的类名
+        String className = joinPoint.getTarget().getClass().getName();
+        // 获取请求的方法名
+        String methodName = method.getName();
+        methodName = className + "." + methodName;
+        // 请求方式
+        operationLog.setRequestMethod(Objects.requireNonNull(request).getMethod());
+        // 请求方法
+        operationLog.setOptMethod(methodName);
+        // 请求参数
 //        operationLog.setRequestParam(JSON.toJSONString(joinPoint.getArgs()));
 //        // 返回结果
 //        operationLog.setResponseData(JSON.toJSONString(keys));
@@ -100,11 +101,11 @@ package com.luqiyu.qiyublogspringboot.handler;
 //        String ipAddr = IpUtil.getIpAddr(request);
 //        operationLog.setIpAddr(ipAddr);
 //        operationLog.setIpSource(IpUtil.getIpSource(ipAddr));
-//        // 请求URL
-//        operationLog.setOptUrl(request.getRequestURI());
-//        // 创建时间
-//        operationLog.setCreateTime(new Date());
-//        operationLogMapper.insert(operationLog);
-//
-//    }
-//}
+        // 请求URL
+        operationLog.setOptUrl(request.getRequestURI());
+        // 创建时间
+        operationLog.setCreateTime(new Date());
+        operationLogMapper.insert(operationLog);
+
+    }
+}
