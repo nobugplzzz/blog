1818e7da8bc2b7cba20d293a80d0b7d8c10834f0
luqiyu <316882012@qq.com>
2021-06-14 17:39:16 +0800

    新增前台页面

---
+++ qiyu-blog-springboot/src/main/java/com/luqiyu/qiyublogspringboot/dto/ArchiveDTO.java
@@ -0,0 +1,7 @@
+package com.luqiyu.qiyublogspringboot.dto;/**
+* s
+* @author: 启誉
+* @create: 2021-06-14 
+**/
+public class ArchiveDTO {
+}
---
+++ qiyu-blog-springboot/src/main/java/com/luqiyu/qiyublogspringboot/dto/CategoryDTO.java
@@ -0,0 +1,7 @@
+package com.luqiyu.qiyublogspringboot.dto;/**
+* 分类视图对象，用于前台展示分类
+* @author: 启誉
+* @create: 2021-06-14 
+**/
+public class CategoryDTO {
+}
